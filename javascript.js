10cd400ec00e75027a1fc819c80827e45f1ec1cd
kaosker <thorup93@hotmail.com>
2023-02-06 18:59:52 +0100

    keep working

--- javascript.js
+++ javascript.js
@@ -5,7 +5,18 @@
 // **before the first vowel** to the **end of the word** then add 'ay'
 
 function pigLatinWord(word) {
-    let vowels = "aeiou";
+    if (word[0] === "a" || word[0] === "e" ||
+        word[0] === "i" || word[0] === "o" || word[0] === "u") {
+            return word + "yay";
+        } else {
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] !== "a" || word[i] !== "e" ||
+                word[i] !== "i" || word[i] !== "o" || word[i] !== "u") {
+                    return word.slice(word[i]) + "ay";
+                }
+            }
+
+        }
 
 
 
@@ -25,9 +36,3 @@ Write a function pigLatinWord that takes in a word string and translates the wor
 For this problem use the String.slice() method. The slice() method extracts a section of a string and returns it as a new string, without modifying the original string.
 
 Hint: Remember the String.includes method!
-
-
-console.log(pigLatinWord("apple")); //=> "appleyay"
-console.log(pigLatinWord("eat")); //=> "eatyay"
-console.log(pigLatinWord("banana")); //=> "ananabay"
-console.log(pigLatinWord("trash")); //=> "ashtray"
